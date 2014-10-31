322464672c972bc75e9f16373031ccc0b219414e
Ara Juljulian <ara@juljulian.com>
2014-10-30 17:40:46 -0700

    saving wish lists to file.

--- wishlist.py
+++ wishlist.py
@@ -1,9 +1,27 @@
+
+# This script reads a list of email addresses from a file, then for each email address it
+# 1) loads an Amazon web page
+# 2) submits the email address through a form
+# 3) searches the resulting page for wish list IDs
+# 4) calls a url with the wish list ID and retrieves the corresponding wish list
+# 5) appends the email address, wish list ID, and wish list to a file
+#
+# The script assumes that you have deployed the php files from https://github.com/doitlikejustin/amazon-wish-lister
+# onto a server to be used to retrieve the wish list using a wish list ID.
+
 import mechanize
 import cookielib
 import re
 
+# URL of an Amazon page containing a form that can be used to retrieve wish lists based on email addresses.
+AMAZON_WISHLIST_EMAIL_URL = 'http://www.amazon.com/gp/registry/search.html?ie=UTF8&type=wishlist'
+
+# URL used to retrieve Amazon wish lists from wish list IDs (see https://github.com/doitlikejustin/amazon-wish-lister)
+AMAZON_WISHLIST_ID_URL = 'http://localhost:8888/wishlist.php/?id='
+
 def main():
 
+    # Configure mechanize.  It's used for web page scraping and site crawling.
     br = mechanize.Browser()
     cj = cookielib.LWPCookieJar()
     br.set_cookiejar(cj)
@@ -14,14 +32,44 @@ def main():
     br.set_handle_referer(True)
     br.set_handle_robots(False)
 
+    # Read list of emails.  Assume one email per line.
+    infile = open("emails.txt", 'r')
+    emails = infile.readlines()
+
+    f = open('wishlist.txt', 'w+')
+
+    for email in emails:
+
+        # Remove the newline character from the end of the email.
+        email = email.replace('\n', '')
+
+        # Get the wish list ID from the email address if it exists.
+        # If the wish list ID exists, then retrieve the corresponding wish list as well.
+        wishlist_id = retrieve_wishlist_id(email, br)
+
+        if wishlist_id == "":
+            wishlist = ""
+        else:
+            wishlist = retrieve_wishlist(wishlist_id, br)
+
+        wishlist_entry = email + " (" + wishlist_id + "): " + wishlist
+
+        print(wishlist_entry)
+
+        f.write(wishlist_entry)
+        f.write('\n')
+
+    f.close()
+
+def retrieve_wishlist_id(email, br):
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-    r= br.open('http://www.amazon.com/gp/registry/search.html?ie=UTF8&type=wishlist')
+    r= br.open(AMAZON_WISHLIST_EMAIL_URL)
 
     # Select the second (index one) form
     form = br.select_form(nr=1)
 
-    br.form['field-name'] = 'ajuljulian@yahoo.com'
+    br.form['field-name'] = email
     response1 = br.submit()
 
     response1_data = response1.get_data()
@@ -29,8 +77,23 @@ def main():
     match = re.search(r'action="/gp/registry/wishlist/([a-zA-Z0-9]+)', response1_data)
 
     if match:
-        print 'found', match.group(1)
+        # Use a back reference to get the wish list.
+        wishlistID = match.group(1)
+        print 'found', wishlistID
+        return wishlistID
     else:
         print 'did not find'
+        return ""
+
+
+def retrieve_wishlist(wishlist_id, br):
+
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    r= br.open(AMAZON_WISHLIST_ID_URL + wishlist_id)
+
+    html = r.read()
+
+    return html
 
 main()
